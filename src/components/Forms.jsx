fe5b1216537ffd9502f246c4296cf31a37a1421d
tohirov2002 <tohirovahmad1519@gmail.com>
2024-05-01 23:57:10 +0500

    yangilandi

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { TbXboxX } from "react-icons/tb";
 
-const Forms = () => {
+const Forms = ({openModal,openFunk}) => {
   return (
     <section className='container'>
-        <div className='flex items-center justify-center flex-col'>
+        <div className={openModal ? 'show1': 'info'}>
+        <div className='flex items-center justify-center flex-col bg-white p-[50px] w-full max-w-[1100px] rounded-3xl'>
+            <div className='absolute right-[30px] top-[20px]'>
+               <TbXboxX onClick={openFunk} className='w-[35px] h-[35px] text-[#2D3663] cursor-pointer'/>
+            </div>
             <div className='flex items-center'>
                 <h1 className='text-[#2D3663] text-[40px]'>Мутахассис кўригига ёзилиш</h1>
-                <TbXboxX className='w-[30px] h-[30px] text-[#2D3663]'/>
             </div>
             <div className='flex items-center justify-between mt-[30px] gap-[50px] w-full max-w-[950px]'>
                 <input type="text" placeholder='Исм' className='text-[#2D3663] w-full max-w-[950px] bg-blue-50 rounded-[20px] pt-3 pb-3 pl-4'/>
@@ -27,6 +30,7 @@ const Forms = () => {
             <h1 className='text-[#62667c] font-bold mt-[20px]'>Кимга рўйхатдан ўтишни танланг</h1>
             <button  className='flex items-center justify-center gap-3 text-white rounded-3xl w-[250px] pt-3 pb-3 mt-[30px] ml-4 bg-blue-950 hover:bg-slate-900'>Қабулга ёзилиш</button>
     
+        </div>
         </div>
     </section>
   )
