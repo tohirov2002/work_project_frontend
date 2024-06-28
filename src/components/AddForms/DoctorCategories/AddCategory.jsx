d6bd4a8c18d14bbe737ead286c0127742ffd87f4
tohirov2002 <tohirovahmad1519@gmail.com>
2024-06-28 10:04:39 +0500

    yangilandi

--- src/components/AddForms/DoctorCategories/AddCategory.jsx
+++ src/components/AddForms/DoctorCategories/AddCategory.jsx
@@ -14,7 +14,7 @@ const AddComments = ({ showModal, handleShow , handleData}) => {
   useEffect(() => {
     const handleData = async () => {
       try {
-        const response = await axios.get('http://127.0.0.1:8000/api/comments/')
+        const response = await axios.get('http://127.0.0.1:8000/api/doctor/category/')
         setData(response.data)
       } catch (error) {
         console.error(error)
@@ -22,29 +22,20 @@ const AddComments = ({ showModal, handleShow , handleData}) => {
     }
     handleData()
   }, [])
-
-
-
-
-
  
 
   // data create
   const [contentInp, setContentInp] = useState("");
-  const [descriptionRu, setDescriptionRu] = useState("");
-  const [ImageInp, setImageInp] = useState(null);
 
   const onCreate = (e) => {
     e.preventDefault();
 
     const formData = new FormData();
-    formData.append('description', contentInp);
-    formData.append('description_ru', descriptionRu);
-    formData.append('image', ImageInp);
+    formData.append('category_name', contentInp);
 
     console.log("FormData:", formData);
 
-    axios.post('http://127.0.0.1:8000/api/comments/', formData, {
+    axios.post('http://127.0.0.1:8000/api/doctor/category/', formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
@@ -53,8 +44,6 @@ const AddComments = ({ showModal, handleShow , handleData}) => {
         console.log("Response:", response);
         handleData();
         setContentInp("");
-        setDescriptionRu("");
-        setImageInp(null);
       })
       .catch(function (error) {
         if (error.response) {
@@ -70,10 +59,6 @@ const AddComments = ({ showModal, handleShow , handleData}) => {
       });
   }
 
-  const handleFileChange = (e) => {
-    setImageInp(e.target.files[0]);
-  }
-
   return (
     <div className={showModal? 'column': 'info'}>
       <div className='columns'>
@@ -81,11 +66,9 @@ const AddComments = ({ showModal, handleShow , handleData}) => {
         <div className='bg-blue-500 w-[30px] cursor-pointer h-[30px] rounded-[50%] flex items-center justify-center'>
             <ImCross onClick={handleShow} className='text-white'/>
         </div>
-        <h1 className='text-[#2D3663] text-[40px] '>Sharx qo'shish</h1>
+        <h1 className='text-[#2D3663] text-[40px] '>Kategoriya qo'shish</h1>
         <form onSubmit={onCreate}>
-          <MyInput myValue={contentInp} mySetValueChange={setContentInp} myplace='description' type='text'/>
-          <MyInput myValue={descriptionRu} mySetValueChange={setDescriptionRu} myplace='description_ru' type='text'/>
-          <input className='text-[#2D3663] mt-8 w-[550px] bg-blue-50 rounded-[20px] pt-3 pb-3 pl-4' type="file" onChange={handleFileChange} />
+          <MyInput myValue={contentInp} mySetValueChange={setContentInp} myplace='category name' type='text'/>
           <button onClick={handleShow} type="submit" className='ml-[150px] flex items-center justify-center gap-3 text-white rounded-3xl w-[250px] pt-3 pb-3 mt-[30px] bg-blue-950 hover:bg-slate-900'>
             Submit
           </button>
